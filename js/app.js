84717311863b111f14db60a4f3a65377faa2fca8
Alex Shyshkov <shyshkov.alex@gmail.com>
2019-12-17 19:53:04 +0200

    Add application script with MVC pattern

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
 'use strict';
 
+class Model{
+
+}
+
+class View{
+
+}
+
+class Controller{
+	constructor(model, view){
+		this.model = model;
+		this.view = view;
+	}
+}
+
+let model = new Model();
+let view = new View();
+let todoApp = new Controller(model, view);
