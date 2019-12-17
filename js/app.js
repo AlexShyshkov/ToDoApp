29285c311b7812e884ea4e1521d6084d6626d4a4
Alex Shyshkov <shyshkov.alex@gmail.com>
2019-12-17 20:49:13 +0200

    Add editTodoItem function to the Model class

--- js/app.js
+++ js/app.js
@@ -5,12 +5,32 @@ class Model{
 		this.todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
 	}
 
-	addTodoItems(inputedText){
+	addTodoItem(inputedText){
 		let todoItem = {
 			id: this.todoItems.length > 0 ? this.todoItems[this.todoItems.length - 1].id + 1 : 1,
 			text: inputedText,
 			isComplete: false
 		}
+
+		this.todoItems.push(todoItem);
+	}
+
+	editTodoItem(id, newTodoItemValue){
+		this.todoItems = this.todoItems.map(todoItem =>
+			todoItem.id === id ? {
+				id: todoItem.id,
+				text: newTodoItemValue,
+				isComplete: todoItem.isComplete
+			} :	todoItem
+		)	
+	}
+
+	deleteTodoItem(id){
+
+	}
+
+	changeCompleteProperty(id){
+
 	}
 }
 
