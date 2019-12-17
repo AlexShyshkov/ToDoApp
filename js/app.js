cfca5f8f95b91c79d1ab3f55c1977170e9285fa4
Alex Shyshkov <shyshkov.alex@gmail.com>
2019-12-17 20:15:55 +0200

    Add addTodoItem function to the Model

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 'use strict';
 
 class Model{
+	constructor(){
+		this.todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
+	}
 
+	addTodoItems(inputedText){
+		let todoItem = {
+			id: this.todoItems.length > 0 ? this.todoItems[this.todoItems.length - 1].id + 1 : 1,
+			text: inputedText,
+			isComplete: false
+		}
+	}
 }
 
 class View{
