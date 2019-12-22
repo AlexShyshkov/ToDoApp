4a5ef6e9703c7d11bff5a3cac4863edf6d045c4b
Alex Shyshkov <shyshkov.alex@gmail.com>
2019-12-22 18:30:05 +0200

    Renamed private method _saveItem on _updateValue. Add comment to the blocks of code

--- js/app.js
+++ js/app.js
@@ -9,11 +9,14 @@ class Model{
 		this.onTodoListChanged = callback;
 	}
 
-	_saveItem(todoItems){
+	/*Private methode for updating value in local storage and state of model
+	This method will call after every change of this.todoItems*/
+	_updateValue(todoItems){
 		this.onTodoListChanged(todoItems);
 		localStorage.setItem('items', JSON.stringify(todoItems));
 	}
 
+	//Add task to the task list
 	addTodoItem(inputedText){
 		let todoItem = {
 			id: this.todoItems.length > 0 ? this.todoItems[this.todoItems.length - 1].id + 1 : 1,
@@ -22,9 +25,10 @@ class Model{
 		}
 
 		this.todoItems.push(todoItem);
-		this._saveItem(this.todoItems);
+		this._updateValue(this.todoItems);
 	}
 
+	//Check all tasks in todo items list and replace the text of task with choosen id
 	editTodoItem(id, newTodoItemValue){
 		this.todoItems = this.todoItems.map(todoItem =>
 			todoItem.id === id ? {
@@ -34,15 +38,17 @@ class Model{
 			} :	todoItem
 		);
 
-		this._saveItem(this.todoItems);	
+		this._updateValue(this.todoItems);	
 	}
 
+	//Delete task by id
 	deleteTodoItem(id){
 		this.todoItems = this.todoItems.filter(todoItem => todoItem.id !== id);
 
-		this._saveItem(this.todoItems);
+		this._updateValue(this.todoItems);
 	}
 
+	//Toggle boolean complete property of the task
 	completeTodoItem(id){
 		this.todoItems = this.todoItems.map(todoItem =>
 			todoItem.id === id ? {
@@ -52,21 +58,21 @@ class Model{
 			} : todoItem
 		);
 
-		this._saveItem(this.todoItems);
+		this._updateValue(this.todoItems);
 	}
 }
 
 class View{
 	constructor(){
+		//Recieve root element
 		this.todoApp = this.getElement('#app');
 
-		/*Block with select, input, add button*/
+		//Creation of block with select, input field and add button
 		this.inputBlock = this.createBlock('div', 'input-block');
 		this.inputBlock.id = 'inputBlock';
-
 		this.form = this.createBlock('form');
 
-		/*Select for filter items*/
+		//Creation of select element for filter items in list
 		this.select = this.createBlock('select', 'todo-select');
 		this.select.id = 'taskFilter';
 		this.optionAll = this.createBlock('option');
@@ -77,19 +83,19 @@ class View{
 		this.optionActive.text = 'Active';
 		this.select.append(this.optionAll, this.optionDone, this.optionActive);
 
-		/*Input for todo items*/
+		//Creation of input field for todo items
 		this.input = this.createBlock('input', 'todo-input');
 		this.input.id = 'taskInput';
 		this.input.type = 'text';
 		this.input.placeholder = 'Write you task here...';
 		this.input.name = 'task';
 
-		/*Add button*/
+		//Creation of Add button
 		this.addButton = this.createBlock('button', 'add-btn');
 		this.addButton.id = 'addTaskBtn';
 		this.addButton.textContent = 'Add';
 
-		/*List for todo items*/
+		//Creation of list for todo items
 		this.todoItemsList = this.createBlock('ul', 'todo-list');
 		this.todoItemsList.id = 'todoItemsList';
 
@@ -97,19 +103,21 @@ class View{
 		this.inputBlock.append(this.form);
 		this.todoApp.append(this.inputBlock, this.todoItemsList);
 
-		/**/
 		this._temporaryTodoItem = '';
 		this._updateTemporaryTodoItem();
 	}
 
+	//Private method for recieving value from input
 	get _inputText(){
 		return this.input.value;
 	}
 
+	//Private method for reset/clear input field after addition of new task
 	_clearInput(){
 		this.input.value = '';
 	}
 
+	//Create tag with css style
 	createBlock(tagName, className){
 		let block = document.createElement(tagName);
 		if(className){
@@ -119,22 +127,27 @@ class View{
 		return block;
 	}
 
+	//Recieve element from the DOM
 	getElement(elementId){
 		let element = document.querySelector(elementId);
 
 		return element;
 	}
 
+	//Method creates ul and li tags for new tasks and display list of tasks
 	displayTodoItems(todoItems){
+		//delete all nodes if they exist
 		while(this.todoItemsList.firstChild){
 			this.todoItemsList.removeChild(this.todoItemsList.firstChild);
 		}
 
+		//display default message if no tasks in the list
 		if(todoItems.length === 0){
 			let defaultMessage = this.createBlock('p', 'default-msg');
 			defaultMessage.textContent = 'You have no tasks today';
 			this.todoItemsList.append(defaultMessage);
 		} else {
+			//Create todo item node
 			todoItems.forEach( todoItem => {
 				let line = this.createBlock('li', 'todo-list-line');
 				line.id = todoItem.id;
@@ -146,11 +159,13 @@ class View{
 				let span = this.createBlock('span', 'todo-list-line__span');
 				span.contentEditable = true;
 				
+				//If todo item has status complete - create strike through line text
 				if(todoItem.complete){
 					let strike = this.createBlock('s');
 					strike.textContent = todoItem.text;
 					span.append(strike);
 				} else {
+					//In othe case just display todo item
 					span.textContent = todoItem.text;
 				}
 
@@ -158,13 +173,15 @@ class View{
 				deleteBtn.textContent = 'Delete';
 				line.append(checkbox, span, deleteBtn);
 
+				//Append all nodes to the todo items list, which was created in constructor
 				this.todoItemsList.prepend(line);
 			});
 		}
 
-		console.log(todoItems);
+		//console.log(todoItems);
 	}
 
+	//Private method for updating temporary state
 	_updateTemporaryTodoItem(){
 		this.todoItemsList.addEventListener('input', event => {
 			if(event.target.className === 'todo-list-line__span'){
@@ -173,6 +190,7 @@ class View{
 		});
 	}
 
+	//Add event listeners on the DOM elements
 	bindAddTodoItem(handler){
 		this.form.addEventListener('submit', event => {
 			event.preventDefault();
@@ -223,14 +241,17 @@ class Controller{
 
 		this.model.bindTodoListChanged(this.onTodoListChanged);
 
+		//Call event handlers from the view
 		this.view.bindAddTodoItem(this.addTodoItemHandler);
 		this.view.bindDeleteTodoItem(this.deleteTodoItemHandler);
 		this.view.bindCompleteTodoItem(this.completeTodoItemHandler);
 		this.view.bindEditTodoItem(this.editTodoItemHandler);
 
+		//Display default items if they exist
 		this.onTodoListChanged(this.model.todoItems);
 	}
 
+	//Handlers for the events in yhe controller
 	onTodoListChanged = todoItems => {
 		this.view.displayTodoItems(todoItems);
 	}
